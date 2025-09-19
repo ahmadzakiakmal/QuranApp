af7a64f67635a7cac15124546eade62ba20d0917
ahmadzakiakmal <ahmad.zaki.akmal@gmail.com>
2025-09-19 21:13:39 +0700

    feat: add simple front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,36 @@
+"use client"
+import { useEffect, useState } from "react"
+import { Chapter } from "@/types/api"
+import axios from "axios";
+
 export default function Page() {
+  const [chapters, SetChapter] = useState<Chapter[]>([]); 
+  useEffect(() => {
+    axios.get("/api/quran/chapters")
+      .then((res) => {
+        console.log(res)
+        SetChapter(res.data.chapters)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
+
   return(
     <main>
-      QuranApp
+      <h1><strong>QuranApp</strong></h1>
+
+
+      <h2><strong>Chapters</strong></h2>
+      <div className="grid gap-2 grid-cols-4">
+        {
+          chapters?.map((c) => {
+            return(
+              <a href={"/api/quran/chapters/"+c.id} key={c.id}><div >{c.name_complex}</div></a>
+            )
+          })
+        }
+      </div>
     </main>
   )
 }
--- src/types/api.ts
+++ src/types/api.ts
@@ -10,3 +10,19 @@ export interface ExchangeTokenResponse {
   expires_in: number;
   scope: string;
 }
+
+export interface Chapter {
+  id: number;
+  revelation_place: "makkah" | "madinah";
+  revelation_order: number;
+  bismillah_pre: boolean;
+  name_simple: string;
+  name_complex: string;
+  name_arabic: string;
+  verses_count: number;
+  pages: number[];
+  translated_name: {
+    language_name: string;
+    name: string;
+  };
+}
